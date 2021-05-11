576e4eb3b54260c085d81811d30a5555f70a109b
Kaan <76836631+Seuika@users.noreply.github.com>
2021-05-11 08:53:54 +0300

    Add files via upload

---
+++ Auroa.h
@@ -0,0 +1,8 @@
+#pragma once
+class Auroa{
+public:
+	void StartAuroa();
+	int AuroaMain();
+	void Yazi();
+};
+
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Auroa.h"
+
+int main() {
+	Auroa AU; AU.StartAuroa();
+	return 0;
+}
